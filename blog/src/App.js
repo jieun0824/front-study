64607d882243c6813b59760f8bf3602f73b5caa9
jieun0824 <suspen064@gmail.com>
2023-07-16 22:49:19 +0900

    props state 이용해서 블로그 모달 구현

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+
+
 function App() {
   let post = '강남 우동 맛집';
   let [글제목, b] = useState(['여자 코트 추천', '냉우동 맛집', '강남면옥 후기']);
+
   let [like1, setLike1] = useState(0);
   let [like2, setLike2] = useState(0);
   let [like3, setLike3] = useState(0);
+
+  let states = [like1, like2, like3]
+  let setFuncs = [setLike1, setLike2, setLike3]
+  //UI의 현재 상태를 state로 저장(닫힌상태/열린상태 -> 형식은 상관 없다! 모달창상태 표현만 가능하면 된다.)
+  let [modal, setModal] = useState(false);
+  let [number, setNumber] = useState(0);
+
 
   return (
     <div className="App">
@@ -21,7 +31,7 @@ function App() {
 
 
       }}>가나다순 정렬</button>
-      <div className='list'>
+      {/* <div className='list'>
         <button onClick={()=>{b(['남자 코트 추천', '냉우동 맛집', '강남면옥 후기'])}}>버튼</button>
         <h4>{글제목[0]}<span onClick={()=>{setLike1(like1+1)}}>👍</span>{like1}</h4>
         <p>2월 17일 발행</p>  
@@ -31,16 +41,55 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{글제목[2]}<span onClick={()=>{setLike3(like3+1)}}>👍</span>{like3}</h4>
+        <h4 onClick={()=>{
+          setModal(!modal)
+        }}>{글제목[2]}<span onClick={()=>{setLike3(like3+1)}}>👍</span>{like3}</h4>
         <p>2월 17일 발행</p>
-      </div>
-      <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+      </div> */}
+
+      {
+        
+
+        글제목.map(function(a, i){
+          return (
+            <div className='list' key={i}>
+              <button>버튼</button>
+              <h4 onClick={()=>{
+                  setModal(!modal);
+                  setNumber(i);
+                }}>{a}
+                <span onClick={()=>{setFuncs[i](states[i]+1)}}>👍</span>
+                {states[i]}
+              </h4>
+              <p>2월 17일 발행</p>  
+            </div>
+          )
+
+          
+        })
+
+        
+      }
+
+      {
+          (modal===true) ? <Modal divnum={글제목} 글수정={b} 글번호={number}/> : null //null - 텅 빈 값
+
+      }
+
     </div>
   );
+}
+
+
+function Modal(props){
+  return(
+    <div className='modal'>
+      <h4>{props.divnum[props.글번호]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={()=>{props.글수정(['남자 코트 추천', '냉우동 맛집', '강남면옥 후기'])}}>글수정</button>
+    </div>
+  )
 }
 
 export default App;
