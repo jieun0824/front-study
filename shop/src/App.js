d37bd4a889ed4e03d501079a38e00b1c81afbe4b
jieun0824 <suspen064@gmail.com>
2023-08-03 20:55:00 +0900

    ajax 통신 학습

--- shop/src/App.js
+++ shop/src/App.js
@@ -6,8 +6,9 @@ import {Container, Navbar, Nav} from 'react-bootstrap'
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 import Detail from './routes/Detail';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import data from '../src/data';
+import axios from 'axios';
 
 
 function Card({data, index, image}){
@@ -21,10 +22,22 @@ function Card({data, index, image}){
   }
 
 
+
+
 function App() {
   let navigate = useNavigate();
-  let [shoes] = useState(data);
-
+  let [shoes, setShoes] = useState(data);
+  let [show, setShow] = useState(true);
+  let [loading, setloading] = useState(false);
+  useEffect(()=>{
+    console.log(shoes)
+  }, [shoes])
+  let [count, setCount] = useState(0);
+  function plusCount(){
+    count++;
+    setCount(count);
+    console.log(count)
+  }
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -46,20 +59,48 @@ function App() {
               <div className="row">
                 {
                 shoes.map(function(a, i){
-                  let url = `https://codingapple1.github.io/shop/shoes${i}.jpg`;
+                  let url = `https://codingapple1.github.io/shop/shoes${i+1}.jpg`;
                   return(
                     <Card data={shoes} index={i} image={url}/>
                   )
                 })
                 }
               </div>
+              {show===true?<button className='btn1' onClick={()=>{
+                plusCount();
+                setloading(true);
+
+                  let data = `https://codingapple1.github.io/shop/data${count+1}.json`
+                  console.log(data)
+                  axios.get(data)
+                  .then((result)=>{
+                    //배열, 오브젝트는 복사본 만들기
+                    console.log(result.data)
+                    let copy = [...shoes, ...result.data];
+                    setShoes(copy)
+                    console.log(copy)
+                    setloading(false)
+                  })
+                  .catch((err)=>{
+                    console.log(err)
+                    setloading(false)
+                  })
+
+                  if(count===2){setShow(false)}
+                
+              }}>더보기</button>:null}
+              {loading === true? <span>로딩중입니다.</span>:null}
             </div>
           </>
         }/>
-        <Route path='/detail' element={<Detail/>}/>
+        <Route path='/detail/:id' element={<Detail shoes={shoes}/>}/>
         <Route path='*' element={<>없는페이지요</>}/>
         <Route path='/about' element={<About/>}>
           <Route path='member' element = {<div>멤버임</div>}/>
+        </Route>
+        <Route path='/event' element = {<Event/>}>
+          <Route path='one' element = {<h5>첫 주문시 양배추즙 서비스</h5>}/>
+          <Route path='two' element = {<h5>생일기념 쿠폰받기</h5>}/>
         </Route>
       </Routes>
 
@@ -72,6 +113,14 @@ function About(){
   return(
     <div>
       <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+function Event(){
+  return(
+    <div>
+      <h5>오늘의 이벤트</h5>
       <Outlet></Outlet>
     </div>
   )
--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,14 +1,51 @@
-function Detail(){
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+function Detail(props){
+  let {id} = useParams(); //현재 url의 파라미터 정보가 이 자리에 남는다.(유저가 :id 자리에 적은거 가져와줌)
+  let currentShoes = props.shoes.find((a)=>{
+    return a.id===parseInt(id)
+  })
+  let [view, setView] = useState(true);
+  let [message, setmessage] = useState('');
+  function saveMessage(e){
+    setmessage(e.target.value)
+    console.log(message)
+  };
+
+  useEffect(()=>{
+    // let a = setTimeout(()=>{
+    //   setView(false)
+    // }, 2000)
+    // return ()=>{
+    //   clearTimeout(a)
+    // }
+    if (isNaN(message) === true){
+      alert('숫자를 입력하세요')
+    }
+  }, [message])
+
+  let [count, setCount] = useState(0);
+  let image = `https://codingapple1.github.io/shop/shoes${parseInt(id)+1}.jpg`
   return(
+
     <div className="container">
+     {
+      view === true ? <div className='alert alert-warning'>
+      2초이내 구매시 할인
+      </div> : null
+     } 
+      {count}
+      <button onClick={()=>{setCount(count++)}}>버튼</button>
   <div className="row">
     <div className="col-md-6">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      <img src={image} width="100%" />
     </div>
     <div className="col-md-6">
-      <h4 className="pt-5">상품명</h4>
-      <p>상품설명</p>
-      <p>120000원</p>
+      <input className="inputBox" onChange={saveMessage}/>
+      <h4 className="pt-5">{currentShoes.title}</h4>
+      <p>{currentShoes.content}</p>
+      <p>{currentShoes.price}</p>
       <button className="btn btn-danger">주문하기</button> 
     </div>
   </div>
