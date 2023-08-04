6d4f64f9e6d1005d46abb1e5c8b7414fc55f3e4c
jieun0824 <suspen064@gmail.com>
2023-08-04 12:53:31 +0900

    transition 연습

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components'
+import Nav from 'react-bootstrap/Nav';
+
 function Detail(props){
   let {id} = useParams(); //현재 url의 파라미터 정보가 이 자리에 남는다.(유저가 :id 자리에 적은거 가져와줌)
   let currentShoes = props.shoes.find((a)=>{
@@ -12,6 +14,8 @@ function Detail(props){
     setmessage(e.target.value)
     console.log(message)
   };
+  let [text, setText] = useState(0);
+  let [opacity, setOpacity] = useState('');
 
   useEffect(()=>{
     // let a = setTimeout(()=>{
@@ -20,16 +24,24 @@ function Detail(props){
     // return ()=>{
     //   clearTimeout(a)
     // }
+
     if (isNaN(message) === true){
       alert('숫자를 입력하세요')
     }
   }, [message])
 
+  useEffect(()=>{
+    setTimeout(()=>{setOpacity('end')}, 100)
+    return(()=>{
+      setOpacity('');
+    })
+  },[])
+
   let [count, setCount] = useState(0);
   let image = `https://codingapple1.github.io/shop/shoes${parseInt(id)+1}.jpg`
   return(
 
-    <div className="container">
+    <div className={"container start " + opacity}>
      {
       view === true ? <div className='alert alert-warning'>
       2초이내 구매시 할인
@@ -49,8 +61,44 @@ function Detail(props){
       <button className="btn btn-danger">주문하기</button> 
     </div>
   </div>
+  <div className="tab">
+    <Nav variant="tabs" defaultActiveKey="link0"> 
+      <Nav.Item onClick={()=>{
+      setText(0)
+     }}>
+        <Nav.Link eventKey="link0">버튼1</Nav.Link>
+      </Nav.Item>
+      <Nav.Item onClick={()=>{
+      setText(1)
+     }}> 
+        <Nav.Link eventKey="link1">버튼2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item onClick={()=>{
+      setText(2)
+     }}>
+        <Nav.Link eventKey="link2">
+        버튼3
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+    <Tabcontent text = {text}/>
+  </div>
 </div> 
   )
 }
+
+function Tabcontent({text}){
+  let [fade, setFade] = useState('')
+  useEffect(()=>{
+    setTimeout(()=>{setFade('end')},100)
+    return(
+      setFade('')
+    )
+  },[text])
+  return (<div className={`start ${fade}`}>
+    {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][text]}
+  </div>)
+}
+
 
 export default Detail;
