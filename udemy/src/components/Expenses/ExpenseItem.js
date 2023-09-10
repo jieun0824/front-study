d870246ccf737e26136fcfb7c069e3e14c3b27f0
jieun0824 <suspen064@gmail.com>
2023-09-10 13:09:18 +0900

    기본 학습 완료

--- udemy/src/components/Expenses/ExpenseItem.js
+++ udemy/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,16 @@
 import './ExpenseItem.css';
-
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car INsurance';
-  const expenseAmount = 294.67;
+
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+    <Card className="expense-item">  
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
